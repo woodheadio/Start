55d83836fe86369e4b71c243c619b8a98bd65eb3
Alex Woodhead <alexander.james.woodhead@gmail.com>
2017-09-05 12:27:19 +0200

    Ignored output from build process in /dist and tweaked webpack production config adding a few plugins run at build.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,28 @@
+import webpack from 'webpack';
 import merge from 'webpack-merge';
 import path from 'path';
 import common from './webpack.common.js';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import UglifyJsWebpackPlugin from 'uglifyjs-webpack-plugin';
 
 export default merge(common, {
+  devtool: 'source-map',
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new UglifyJsWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Caching'
+    }),
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE.ENV': JSON.stringify('production')
+      }
+    })
+  ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'src'),
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/'
   }
 });
